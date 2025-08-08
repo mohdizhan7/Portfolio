9c1a34a17e9f55c1f9ce1b943258d51a273ad32e
Mohd Izhan Shaikh <mohdizhan@Mohds-MacBook-Pro.local>
2025-08-08 11:25:05 +0530

    build: update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // 👈 your repo name here
+  base: '/Portfolio/', // repo name, case-sensitive
   plugins: [react()],
 })
